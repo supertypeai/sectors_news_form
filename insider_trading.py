7daa84a636b56dbbd05978756eeaa97b2a3c9267
AurelliaChristie <aurelliachristie77@gmail.com>
2024-07-05 17:23:04 +0700

    add insider trading form

---
+++ insider_trading.py
@@ -0,0 +1,84 @@
+import streamlit as st
+from datetime import datetime as dt
+import requests
+
+# data
+api_key = st.secrets["API_KEY"]
+
+available_subsectors = [
+  "alternative-energy",
+  "apparel-luxury-goods",
+  "automobiles-components",
+  "banks",
+  "basic-materials",
+  "consumer-services",
+  "financing-service",
+  "food-beverage",
+  "food-staples-retailing",
+  "heavy-constructions-civil-engineering",
+  "healthcare-equipment-providers",
+  "holding-investment-companies",
+  "household-goods",
+  "industrial-goods",
+  "industrial-services",
+  "insurance",
+  "investment-service",
+  "leisure-goods",
+  "logistics-deliveries",
+  "media-entertainment",
+  "multi-sector-holdings",
+  "nondurable-household-products",
+  "oil-gas-coal",
+  "pharmaceuticals-health-care-research",
+  "properties-real-estate",
+  "retailing",
+  "software-it-services",
+  "technology-hardware-equipment",
+  "telecommunication",
+  "tobacco",
+  "transportation",
+  "transportation-infrastructure",
+  "utilities"
+]
+
+
+# helper functions
+def format_option(option):
+    return option.replace("-", " ").title()
+
+def post():
+    if not st.session_state.source or not st.session_state.subsector or not st.session_state.file:
+        st.toast("Please fill out the required fields.")
+    else:
+        files = {
+            'file': (st.session_state.file.name, st.session_state.file, 'application/pdf'),
+            'source': (None, st.session_state.source, 'text/plain'),
+            'sub_sector': (None, st.session_state.subsector, 'text/plain')
+        }
+
+        headers = {
+            "Authorization": f"Bearer {api_key}"
+        }
+
+        response = requests.post("https://sectors-news-endpoint.vercel.app/pdf", headers = headers, files=files)
+
+        if response.status_code == 200:
+            st.toast("Insider trading submitted successfully! 🎉")
+            st.session_state.source=""
+            st.session_state.subsector=available_subsectors[0]
+        else:
+            # Handle error
+            st.error(f"Error: Something went wrong. Please try again.")
+  
+# app
+st.title("Sectors News")
+
+insider = st.form('insider')
+
+insider.subheader("Post Insider Trading")
+insider.caption(":red[*] _required_")
+
+file = insider.file_uploader("Upload File (.pdf):red[*]", type="pdf", accept_multiple_files=False, key="file")
+source = insider.text_input("Source:red[*]", placeholder="Enter URL", key="source")
+subsector = insider.selectbox("Subsector:red[*]", options = available_subsectors, format_func=format_option, key="subsector")
+submit = insider.form_submit_button("Submit", on_click=post)
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 pages = {
     "Sectors News": [
-        st.Page("create_news.py", title="Create News", icon="📋"),
+        st.Page("create_news.py", title="Post News", icon="📋"),
+        st.Page("insider_trading.py", title="Post Insider Trading", icon="📂"),
         st.Page("edit_news.py", title="Edit News", icon="✏️")
     ]
 }
