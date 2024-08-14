5b30ad924a673c7f8806d5a3a86ece9b9f481f07
AurelliaChristie <aurelliachristie77@gmail.com>
2024-08-14 22:36:09 +0700

    handle duplicate news submission

--- edit_news.py
+++ edit_news.py
@@ -60,37 +60,37 @@ def format_option(option):
     return option.replace("-", " ").title()
 
 def edit():
-    selected_id = st.session_state.id
+    selected_id = st.session_state.edit_id
     if selected_id != None:
-        st.session_state.view = "view2"
+        st.session_state.view_edit = "view2"
         prev_data = next((item for item in data if item["id"] == selected_id), None)
         if prev_data:
-            st.session_state.title=prev_data["title"]
-            st.session_state.body=prev_data["body"]
-            st.session_state.source=prev_data["source"]
-            st.session_state.date=dt.strptime(prev_data["timestamp"], "%Y-%m-%dT%H:%M:%S").date()
-            st.session_state.time=dt.strptime(prev_data["timestamp"], "%Y-%m-%dT%H:%M:%S").time()
+            st.session_state.edit_title=prev_data["title"]
+            st.session_state.edit_body=prev_data["body"]
+            st.session_state.edit_source=prev_data["source"]
+            st.session_state.edit_date=dt.strptime(prev_data["timestamp"], "%Y-%m-%dT%H:%M:%S").date()
+            st.session_state.edit_time=dt.strptime(prev_data["timestamp"], "%Y-%m-%dT%H:%M:%S").time()
             st.session_state.subsector=prev_data["sub_sector"]
-            st.session_state.tags=", ".join(prev_data["tags"])
-            st.session_state.tickers=", ".join(prev_data["tickers"])
+            st.session_state.edit_tags=", ".join(prev_data["tags"])
+            st.session_state.edit_tickers=", ".join(prev_data["tickers"])
     else:
         st.toast("Please select 1 id.")
 
 def post():
-    if not st.session_state.source or not st.session_state.date or not st.session_state.time or not st.session_state.subsector or not st.session_state.tags or not st.session_state.tickers:
+    if not st.session_state.edit_source or not st.session_state.edit_date or not st.session_state.edit_time or not st.session_state.subsector or not st.session_state.edit_tags or not st.session_state.edit_tickers:
         st.toast("Please fill out the required fields.")
     else:
         # process form data
-        selected_id = st.session_state.id
-        tags_list = [tag.strip() for tag in st.session_state.tags.split(',') if tag.strip()]
-        tickers_list = [ticker.strip() for ticker in st.session_state.tickers.split(',') if ticker.strip()]
+        selected_id = st.session_state.edit_id
+        tags_list = [tag.strip() for tag in st.session_state.edit_tags.split(',') if tag.strip()]
+        tickers_list = [ticker.strip() for ticker in st.session_state.edit_tickers.split(',') if ticker.strip()]
 
         data = {
             "id": selected_id,
-            "title": st.session_state.title,
-            "body": st.session_state.body,
-            "source": st.session_state.source,
-            "timestamp": dt.combine(st.session_state.date, st.session_state.time).strftime("%Y-%m-%d %H:%M:%S"),
+            "title": st.session_state.edit_title,
+            "body": st.session_state.edit_body,
+            "source": st.session_state.edit_source,
+            "timestamp": dt.combine(st.session_state.edit_date, st.session_state.edit_time).strftime("%Y-%m-%d %H:%M:%S"),
             "sub_sector": st.session_state.subsector,
             "tags": tags_list,
             "tickers": tickers_list
@@ -104,34 +104,34 @@ def post():
 
         if response.status_code == 200:
             st.toast("News editted successfully! 🎉")
-            st.session_state.title=""
-            st.session_state.body=""
-            st.session_state.source=""
-            st.session_state.date=dt.today()
-            st.session_state.time=dt.now()
+            st.session_state.edit_title=""
+            st.session_state.edit_body=""
+            st.session_state.edit_source=""
+            st.session_state.edit_date=dt.today()
+            st.session_state.edit_time=dt.now()
             st.session_state.subsector=available_subsectors[0]
-            st.session_state.tags=""
-            st.session_state.tickers=""
-            st.session_state.view = 'view1'
+            st.session_state.edit_tags=""
+            st.session_state.edit_tickers=""
+            st.session_state.view_edit = 'view1'
         else:
             # Handle error
             st.error(f"Error: Something went wrong. Please try again.")
 
 def back():
-    st.session_state.view = "view1"
+    st.session_state.view_edit = "view1"
 
 # app
-if 'view' not in st.session_state:
-    st.session_state.view = 'view1'
+if 'view_edit' not in st.session_state:
+    st.session_state.view_edit = 'view1'
 
 st.title("Sectors News")
 
-if st.session_state.view == "view1":
+if st.session_state.view_edit == "view1":
     st.subheader("Edit News")
 
     if (len(data) > 0):
         form = st.form("edit")
-        selected_id = form.selectbox("Select id", [i['id'] for i in sorted(data, key=lambda x: x["id"])], key="id")
+        selected_id = form.selectbox("Select id", [i['id'] for i in sorted(data, key=lambda x: x["id"])], key="edit_id")
         form.form_submit_button("Edit", type="primary", on_click=edit)
 
         st.dataframe(sorted(data, key=lambda x: x["id"], reverse=True), 
@@ -142,20 +142,20 @@ if st.session_state.view == "view1":
         st.info("There is no news in the database.")
         st.page_link("create_news.py", label="Create News", icon=":material/arrow_back:")
 
-elif st.session_state.view == "view2":
+elif st.session_state.view_edit == "view2":
     edit_news = st.form('edit_news')
 
     edit_news.subheader("Edit News")
     back_button = edit_news.form_submit_button("< Back", on_click=back)
     edit_news.caption(":red[*] _required_")
-    id = edit_news.text_input("ID*", value= st.session_state.get("id", ""), disabled=True, key="id")
-    title = edit_news.text_input("Title", value=st.session_state.get("title", ""), placeholder="Enter title", key="title")
-    body = edit_news.text_area("Body", value=st.session_state.get("body", ""), placeholder="Enter body", key="body")
-    source = edit_news.text_input("Source:red[*]", value=st.session_state.get("source", ""), placeholder="Enter URL", key="source")
-    date = edit_news.date_input("Created Date (GMT+7):red[*]", value=st.session_state.get("date", dt.today()), max_value=dt.today(), format="YYYY-MM-DD", key="date")
-    time = edit_news.time_input("Created Time (GMT+7)*:red[*]", value=st.session_state.get("time", dt.now().time()), key="time", step=60)
+    id = edit_news.text_input("ID*", value= st.session_state.get("edit_id", ""), disabled=True, key="edit_id")
+    title = edit_news.text_input("Title", value=st.session_state.get("edit_title", ""), placeholder="Enter title", key="edit_title")
+    body = edit_news.text_area("Body", value=st.session_state.get("edit_body", ""), placeholder="Enter body", key="edit_body")
+    source = edit_news.text_input("Source:red[*]", value=st.session_state.get("edit_source", ""), placeholder="Enter URL", key="edit_source")
+    date = edit_news.date_input("Created Date (GMT+7):red[*]", value=st.session_state.get("edit_date", dt.today()), max_value=dt.today(), format="YYYY-MM-DD", key="edit_date")
+    time = edit_news.time_input("Created Time (GMT+7)*:red[*]", value=st.session_state.get("edit_time", dt.now().time()), key="edit_time", step=60)
     subsector = edit_news.selectbox("Subsector:red[*]", options=available_subsectors, index=available_subsectors.index(st.session_state.get("subsector", available_subsectors[0])), format_func=format_option, key="subsector")
-    tags = edit_news.text_area("Tags:red[*]", value=st.session_state.get("tags", ""), placeholder="Enter tags separated by commas, e.g. idx, market-cap", key="tags")
-    tickers = edit_news.text_area("Tickers:red[*]", value=st.session_state.get("tickers", ""), placeholder="Enter tickers separated by commas, e.g. BBCA.JK, BBRI.JK", key="tickers")
+    tags = edit_news.text_area("Tags:red[*]", value=st.session_state.get("edit_tags", ""), placeholder="Enter tags separated by commas, e.g. idx, market-cap", key="edit_tags")
+    tickers = edit_news.text_area("Tickers:red[*]", value=st.session_state.get("edit_tickers", ""), placeholder="Enter tickers separated by commas, e.g. BBCA.JK, BBRI.JK", key="edit_tickers")
 
     submit2 = edit_news.form_submit_button("Submit", on_click=post, type="primary")        
